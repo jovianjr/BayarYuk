4090c5b7ec845e25cca2e8ba1d7f8b0b0377f0c3
jovian <alphonsusjovian@gmail.com>
2023-05-23 11:16:51 +0700

    feat: add tailwind

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Fiorenza Celestyn, Ipung Sonia Faradhisma, Agnes Suarna, Alphonsus Jovian J.R.">
+    <title>Bayar Yuk</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
