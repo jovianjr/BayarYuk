47be902323a8ef332fea49dc2a0d771655f1c18a
clstyn <fiorenzacelestyn@gmail.com>
2023-06-02 20:39:55 +0700

    hotfix: number format, header qr

--- resources/views/qr/index.blade.php
+++ resources/views/qr/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
+@include('includes.header', ['pageTitle'=>'QR Code', 'back'=>'/'])
 <div class="w-full h-full flex flex-col gap-4 items-center justify-center px-[5vw] py-[12.5vh]">
     <div class="relative flex justify-center items-center w-full h-full bg-[url('/images/bg-qr.png')] bg-cover border border-c-pink rounded-xl">
         <span class="absolute top-4 left-4 text-white font-semibold">{{ $customer->name }}</span>
