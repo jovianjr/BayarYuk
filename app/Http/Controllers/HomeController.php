6ba36594dfdf3eb73025bd7bf54f995f0a821f17
jovian <alphonsusjovian@gmail.com>
2023-06-02 17:08:36 +0700

    hotfix(home): fix homepage controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,37 +22,40 @@ class HomeController extends Controller
                 'balance' => $user->balance
             ];
 
-        $transactions = Transaction::select([
-            'amount',
-            'created_at',
-            DB::raw('"transfer" as type'),
-            DB::raw('IF(from_account_id = "' . $user->id . '", "OUT", IF(to_account_id = "' . $user->id . '", "IN", "")) as status'),
-            DB::raw("DATE_FORMAT(created_at, '%d %M %Y') AS date"),
-            DB::raw("DATE_FORMAT(created_at, '%H:%i') AS time")
-        ])
-            ->where('from_account_id', $user->id)
-            ->orWhere('to_account_id', $user->id);
+            $transactions = Transaction::select([
+                'amount',
+                'created_at',
+                DB::raw('"transfer" as type'),
+                DB::raw('IF(from_account_id = "' . $user->id . '", "OUT", IF(to_account_id = "' . $user->id . '", "IN", "")) as status'),
+                DB::raw("DATE_FORMAT(created_at, '%d %M %Y') AS date"),
+                DB::raw("DATE_FORMAT(created_at, '%H:%i') AS time")
+            ])
+                ->where('from_account_id', $user->id)
+                ->orWhere('to_account_id', $user->id);
 
-        $payments = Payment::select([
-            'amount',
-            'created_at',
-            DB::raw('"payment" as type'),
-            DB::raw('IF(from_account_id = "' . $user->id . '", "OUT", IF(to_account_id = "' . $user->id . '", "IN", "")) as status'),
-            DB::raw("DATE_FORMAT(created_at, '%d %M %Y') AS date"),
-            DB::raw("DATE_FORMAT(created_at, '%H:%i') AS time")
-        ])
-            ->where('from_account_id', $user->id)
-            ->orWhere('to_account_id', $user->id);
+            $payments = Payment::select([
+                'amount',
+                'created_at',
+                DB::raw('"payment" as type'),
+                DB::raw('IF(from_account_id = "' . $user->id . '", "OUT", IF(to_account_id = "' . $user->id . '", "IN", "")) as status'),
+                DB::raw("DATE_FORMAT(created_at, '%d %M %Y') AS date"),
+                DB::raw("DATE_FORMAT(created_at, '%H:%i') AS time")
+            ])
+                ->where('from_account_id', $user->id)
+                ->orWhere('to_account_id', $user->id);
 
-        $data =  $transactions->union($payments)
-            ->limit(3)
-            ->orderBy('created_at', 'DESC')
-            ->get();
+            $data =  $transactions->union($payments)
+                ->limit(3)
+                ->orderBy('created_at', 'DESC')
+                ->get();
 
-        if ($user->account_type == 'normal') {
-            return view('homepage', ['customer' => $customerData, 'transactions' => $data]);
-        } else if ($user->account_type == 'partner') {
-            return view('admin.homepage', ['customer' => $customerData, 'transactions' => $data]);
+            if ($user->account_type == 'normal') {
+                return view('homepage', ['customer' => $customerData, 'transactions' => $data]);
+            } else if ($user->account_type == 'partner') {
+                return view('admin.homepage', ['customer' => $customerData, 'transactions' => $data]);
+            }
+        } else {
+            return view('welcome');
         }
     }
 }
