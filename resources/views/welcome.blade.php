eccd035a66ddbc86262b29718e8551e015109357
jovian <alphonsusjovian@gmail.com>
2023-06-04 19:23:59 +0700

    feat(welcome): fix button login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
     <h2 class="text-4xl text-yellow-400 font-bold text-center mb-4">BayarYuk</h2>
     <img src="/images/welcome/ilustrasi-goldy.png" alt="" class="mt-6 aspect-auto w-3/4">
 
-    <button class="bg-c-pink w-1/2 font-semibold text-white px-6 py-2 rounded-xl mt-12">
-        <a href="{{ url('/login')}}" class="text-lg"> Masuk </a>
-    </button>
+    <a href="{{ url('/login')}}" class="bg-c-pink w-1/2 font-semibold text-white px-6 py-2 rounded-xl mt-12 text-lg text-center">
+        Masuk
+    </a>
 </div>
 @endsection
